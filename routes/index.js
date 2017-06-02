fe0d1f4cc51d6c4489c5f25f0057b03031612a4b
Stephen Dawes <sdawes@outlook.com>
2017-06-02 20:30:26 +0100

    Deploy bootstrap utilising stylish portfolio theme. Set up partials and layout and templates. Begin customisation of theme

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hey', message: 'Hello there!' } );
+  res.render('index', { siteName: 'Podeg'} );
 });
 
 module.exports = router;
