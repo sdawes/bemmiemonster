a9515b3c3a7a8c7d1be5a2609a2982ce54ae7e35
Stephen Dawes <sdawes@outlook.com>
2017-06-06 19:09:12 +0100

    Added nodemailer and body-parser packages for sending and parsing form data to an email. Configured a new route for contact form and configured server to send emails using nodemailer to my email.

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./routes/index.js');
+var users = require('./routes/users.js');
+var contact = require('./routes/contact.js');
 
 var app = express();
 
@@ -26,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/contact', contact);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/contact.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+
+router.get('/', function(req, res, next) {
+	res.render('contact');
+});
+
+router.post('/send', function(req, res, next){
+
+	// create reusable transporter object using the default SMTP transport
+	var transporter = nodemailer.createTransport({
+	    host: 'smtp-mail.outlook.com',
+	    port: 587,
+	    secure: false, // secure:true for port 465, secure:false for port 587
+	    tls: {
+	    	ciphers: 'SSLv3'
+	    },
+	    auth: {
+	        user: 'sdawes@outlook.com',
+	        pass: 'password here'
+	    }
+	});
+
+	// setup email data with unicode symbols
+	var mailOptions = {
+	    from: '"Fred Foo 👻" <sdawes@outlook.com>', // sender address
+	    to: 'sdawes@outlook.com', // list of receivers
+	    subject: '😍 New message from your website!', // Subject line
+	    text: '😍 You have a new message from your website with the following details: Name: ' +req.body.first_name + 'Surname: ' + req.body.last_name + 'Email: ' + req.body.email + 'Phone Number: ' + req.body.number + 'Message: ' + req.body.message, // plain text body
+	    html: '<b>😍 You have a new message from your website with the following details: </b><br><ul><li>First Name: ' + req.body.first_name + '</li><li>Surname: ' + req.body.last_name + '</li><li>Email: ' + req.body.email + '</li><li>Phone Number: ' + req.body.number + '</li><li>Message: ' + req.body.message + '</li></ul>' // html body
+	};
+
+	// send mail with defined transport object
+	transporter.sendMail(mailOptions, function(error, info) {
+	    if (error) {
+	        return console.log(error);
+	        res.redirect('/contact');
+	    } else {
+	    	console.log('Message sent: ' + info.response);
+	    	res.redirect('/');
+	    }
+	    
+	});
+});
+
+
+
+module.exports = router;
